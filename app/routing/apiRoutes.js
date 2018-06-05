269630f2caa6ac7e66282f562c7b2dc3997d686b
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-05 14:29:41 -0700

    Push to Heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,37 +11,49 @@ module.exports = function(app) {
     var result = req.body;
     var name = "";
     var photo = "";
-    var score;
+    Array.prototype.diff = function(a) {
+      return this.filter(function(i) {
+        return a.indexOf(i) < 0;
+      });
+    };
+    function add(a, b) {
+      return Math.abs(a + b);
+    }
     console.log("Survey Result is: ", result);
     //code to handle the compatibility logic
     var userScore = result.score;
     var friendsScore = friends.scores;
-    console.log(friends);
-
+    userScore = userScore.map(Number);
     // function compareScores(friends, userScore) {
-      for (var i = 0; i < friends.length; i++) {
-        for (var j = 0; j < userScore.length; j++){
-        console.log('**********', userScore, friends[i].scores);
-        score += Math.abs(friends[i].scores - userScore);
-        console.log(score);
-      }}
+    // for (var i = 0; i < friends.length; i++) {
+    //   for (var j = 0; j < userScore.length; j++) {
+    //     console.log("**********", userScore, friends[i].scores);
+    //     // score = friends[i].scores.diff(userScore);
 
+    //     console.log('$$', score);
+    //     // score = score.reduce(add, 0);
 
-      // friends.forEach(function(friend) {
-      //   userScore.forEach(function(user) {
-      //     console.log('**********', friend, friend.score);
-      //     score += Math.abs(friend.score - userScore);
-      //     console.log('#######', score);
-      //   })
-      // }) 
-
-
-    //   return score;
+    //     // Math.abs(friends[i].scores - userScore);
+    //     console.log(score);
+    //   }
     // }
-    function modalDisplay(score){
-    
+    friends.forEach(function(friend) {
+      var scoreArr = friend.scores.map(function(item, index) {
+        return Math.abs(item - userScore[index]);
+      });
+      //sum of all elements in the array
+      var score = scoreArr.reduce((x, y) => x + y);
+      console.log("$$", score, "^^^^^^^^^", scoreArr);
+    });
+    if (score < 10) {
+      res.json({ name: friends[index].name, photo: friends[index].photo });
     }
-      res.json({ name: friends[0].name, photo: friends[0].photo });
-   
+    // friends.forEach(function(friend) {
+    //   userScore.forEach(function(user) {
+    //     console.log('**********', friend, friend.score);
+    //     score += Math.abs(friend.score - userScore);
+    //     console.log('#######', score);
+    //   })
+    // })
   });
 };

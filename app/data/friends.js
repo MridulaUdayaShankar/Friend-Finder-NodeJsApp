aead2fbfbdf1dbb90f593554bcbc0bad5272feb9
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-04 21:22:08 -0700

    Add logic to handle the scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,26 @@
 var friendsArray = [
-    {
-        "name":"Rachel",
-        "photo":"http://storage.torontosun.com/v1/dynamic_resize/sws_path/suns-prod-images/1297953532006_ORIGINAL.jpg",
-        "scores":[
-            5,
-            2,
-            3,
-            4,
-            5,
-            1,
-            2,
-            3,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Chandler",
-        "photo":"https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/01/14/12/chandler1.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            3,
-            5,
-            5,
-            2,
-            5,
-            4,
-            4
-          ]
-      },
-      {
-        "name":"Monica",
-        "photo":"https://data.whicdn.com/images/299168475/large.jpg",
-        "scores":[
-            5,
-            1,
-            2,
-            2,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Joey",
-        "photo":"https://pbs.twimg.com/media/Bp26s4mCUAEqXpZ.jpg",
-        "scores":[
-            5,
-            4,
-            4,
-            4,
-            5,
-            3,
-            2,
-            5,
-            4,
-            4
-          ]
-      }
-    ]; 
-    
+  {
+    name: "Rachel",
+    photo:
+      "http://storage.torontosun.com/v1/dynamic_resize/sws_path/suns-prod-images/1297953532006_ORIGINAL.jpg",
+    scores: [5, 2, 3, 4, 5, 1, 2, 3, 4, 1]
+  },
+  {
+    name: "Chandler",
+    photo:
+      "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/01/14/12/chandler1.jpg",
+    scores: [5, 1, 4, 3, 5, 5, 2, 5, 4, 4]
+  },
+  {
+    name: "Monica",
+    photo: "https://data.whicdn.com/images/299168475/large.jpg",
+    scores: [5, 1, 2, 2, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Joey",
+    photo: "https://pbs.twimg.com/media/Bp26s4mCUAEqXpZ.jpg",
+    scores: [5, 4, 4, 4, 5, 3, 2, 5, 4, 4]
+  }
+];
+
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,25 +3,45 @@ var friends = require("../data/friends.js");
 module.exports = function(app) {
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json(friends.scores);
   });
   // A POST routes /api/friends
   app.post("/api/friends", function(req, res) {
-   // code to handle incoming survey results
-    var result = req.params.result;
-    console.log("Survey Result is: ", req.params.result);
+    // code to handle incoming survey results
+    var result = req.body;
+    var name = "";
+    var photo = "";
+    var score;
+    console.log("Survey Result is: ", result);
     //code to handle the compatibility logic
-    var yourSurveyResult = [];
-    for(var i=0; i < result.length; i++){
-      yourSurveyResult.push(result[i]);
-    }
-    compatibilityCheck = yourSurveyResult - friendsArray;
-    compatibilityCheck = Math.abs(compatibilityCheck);
-    if (compatibilityCheck < 5)
-    {
-      // display- your match found!
-      // display- modal
-    }
+    var userScore = result.score;
+    var friendsScore = friends.scores;
+    console.log(friends);
 
+    // function compareScores(friends, userScore) {
+      for (var i = 0; i < friends.length; i++) {
+        for (var j = 0; j < userScore.length; j++){
+        console.log('**********', userScore, friends[i].scores);
+        score += Math.abs(friends[i].scores - userScore);
+        console.log(score);
+      }}
+
+
+      // friends.forEach(function(friend) {
+      //   userScore.forEach(function(user) {
+      //     console.log('**********', friend, friend.score);
+      //     score += Math.abs(friend.score - userScore);
+      //     console.log('#######', score);
+      //   })
+      // }) 
+
+
+    //   return score;
+    // }
+    function modalDisplay(score){
+    
+    }
+      res.json({ name: friends[0].name, photo: friends[0].photo });
+   
   });
 };
